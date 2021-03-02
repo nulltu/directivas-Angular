4e14c07b186a66f1fe56f86034ddb97950e3e214
Rusbent Matta <rusbent_matta@intranet.itresources>
2021-03-02 09:50:24 -0300

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directivas';
+
+  colorStyle:any;
+
+  constructor(){
+    this.colorStyle = {
+      color: 'red',
+      fontSize: '24px'
+    }
+  }
+
+  cambiarColor(pColor:string) {
+  switch (pColor) {
+    case 'o':
+      this.colorStyle.color='orange';
+      break;
+      case 'r':
+      this.colorStyle.color='red';
+      break;
+      case 'b':
+      this.colorStyle.color='blue';
+      break;
+   }
+  }
+
 }
